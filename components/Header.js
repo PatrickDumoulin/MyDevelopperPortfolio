e2df4479b8ac999dfd571e311c6730f76fe117ed
PatrickDumoulin <124711026+PatrickDumoulin@users.noreply.github.com>
2024-09-15 15:46:10 -0400

    Progression Home

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,30 @@
+import Image from "next/image";
+
+//next link
+import Link from "next/link";
+
+//components
+import Socials from '../components/Socials';
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+      <div className="container mx-auto flex justify-between items-center">
+        <div>
+          {/* Titre personnalisé */}
+          <Link href={'/'}>
+            <h1 className="text-3xl">
+              <span className="font-bold text-white">Patrick</span>{' '}
+              <span className="font-normal text-gray-300">Dumoulin</span>
+              <span className="text-red-500">.</span>
+            </h1>
+          </Link>
+        </div>
+        {/* Socials */}
+        <Socials />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,33 @@
+//links
+import Link from 'next/link';
+
+//icons
+import {
+  RiYoutubeLine, 
+  RiInstagramLine, 
+  RiFacebookLine, 
+  RiDribbbleLine, 
+  RiPinterestLine,
+  RiGithubLine,
+  RiLinkedinLine
+} from 'react-icons/ri';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex space-x-4">
+      <Link href={'https://github.com/PatrickDumoulin'}>
+        
+          <RiGithubLine />
+        
+      </Link>
+      <Link href={'https://linkedin.com/in/patrick-dumoulin-37274a2aa'}>
+        
+          <RiLinkedinLine />
+        
+      </Link>
+      
+    </div>
+  );
 };
 
 export default Socials;
