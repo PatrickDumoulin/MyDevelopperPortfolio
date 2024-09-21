fed2cda24212d576b363c642940ac3e409e34113
PatrickDumoulin <124711026+PatrickDumoulin@users.noreply.github.com>
2024-09-21 15:47:22 -0400

    Home, about, Services (Facultatif) done

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // icons
 import {
@@ -8,6 +8,12 @@ import {
   FaReact,
   FaWordpress,
   FaFigma,
+  FaAngular,
+  FaGithub,
+  FaBootstrap,
+  FaVuejs,
+  FaMusic,
+  FaUnity
 } from "react-icons/fa";
 
 import {
@@ -15,83 +21,315 @@ import {
   SiFramer,
   SiAdobexd,
   SiAdobephotoshop,
+  SiCsharp,
+  SiAdobepremierepro,
+  SiMicrosoftword,
+  SiMicrosoftazure,
+  SiReact,
+  SiJavascript,
+  SiTypescript,
+  SiJira,
+  SiAdobeaftereffects,
+  SiBlender,
+  SiXamarin,
+  SiAzuredevops,
+  SiYaml
+
 } from "react-icons/si";
 
+import{
+  TbSql,
+} from "react-icons/tb";
 
 //  data
 const aboutData = [
   {
-    title: 'skills',
+    title: "skills",
     info: [
       {
-        title: 'Web Development',
+        title: "C#",
         icons: [
-          <FaHtml5 />,
-          <FaCss3 />,
-          <FaJs />,
-          <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
+          <SiCsharp />
         ],
       },
       {
-        title: 'UI/UX Design',
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+        title: "ASP.NET Core | Xamarin | Unity",
+        icons: [
+          <SiCsharp />, <SiXamarin/>, <FaUnity/>
+        ],
+      },
+      
+      {
+        title: "Angular | React | Next.js | Vue.Js",
+        icons: [
+          <FaAngular />, <SiReact />, <FaVuejs/> 
+        ],
+      },
+      {
+        title: "SQL Server | PostGres SQL",
+        icons: [
+          < TbSql/>,
+        ],
+      },
+      {
+        title: "Azure | DevOps | YAML",
+        icons: [
+          <SiMicrosoftazure />, <SiAzuredevops/>, <SiYaml/>
+        ],
+      },
+      {
+        title: "JavaScript / TypeScript",
+        icons: [
+          <SiJavascript />, <SiTypescript />
+        ],
+      },
+      {
+        title: "Git / Github",
+        icons: [
+          <FaGithub />,
+        ],
+      },
+      {
+        title: "Jira",
+        icons: [
+          <SiJira />,
+        ],
+      },
+      {
+        title: "BootStrap",
+        icons: [
+          <FaBootstrap />,
+        ],
+      },
+      {
+        title: "PremierePro | PhotoShop | AfterEffects ",
+        icons: [
+          <SiAdobepremierepro/>, <SiAdobephotoshop/>, <SiAdobeaftereffects/>
+        ],
+      },
+      
+
+      
+      
+    ],
+  },
+  // {
+  //   title: "awards",
+  //   info: [
+  //     {
+  //       title: "Webby Awards - Honoree",
+  //       stage: "2011 - 2012",
+  //     },
+  //     {
+  //       title: "Adobe Design Achievement Awards - Finalist",
+  //       stage: "2009 - 2010",
+  //     },
+  //   ],
+  // },
+  {
+    title: "experience",
+    info: [
+      {
+        title: "ACEF de Québec",
+        stage: "4 months Internship (June 2024 - October 2024) Developed a full ASP.NET application from the ground up, utilizing a pre-established project architecture and working under the mentorship of a senior architect. The finished project is showcased in the 'My Work' section and is actively deployed on a local server at the ACEF headquarters, where it is used daily.",
       },
     ],
   },
   {
-    title: 'awards',
+    title: "Formations",
     info: [
       {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
-      },
-      {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
-      },
-    ],
-  },
-  {
-    title: 'experience',
-    info: [
-      {
-        title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
-      },
-      {
-        title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
-      },
-      {
-        title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
-      },
-    ],
-  },
-  {
-    title: 'credentials',
-    info: [
-      {
-        title: 'Web Development - ABC University, LA, CA',
-        stage: '2011',
-      },
-      {
-        title: 'Computer Science Diploma - AV Technical Institute',
-        stage: '2009',
-      },
-      {
-        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-        stage: '2006',
+        title: "AEC Secure Application Development ",
+        stage: "Cégep Limoilou 2022-2024. An intense and fast-paced 2-year full-time program designed to perfect our programming skills. Not suitable for beginners.",
       },
     ],
   },
 ];
 
+//components
+import Avatar from "../../components/Avatar";
+import Circles from "../../components/Circles";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
+//counter
+import CountUp from 'react-countup'
+
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0);
+  console.log(index);
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+      <Circles />
+      {/*Avatar img*/}
+      <motion.div
+        variants={fadeIn("right", 0.2)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="hidden xl:flex absolute  -left-[350px]"
+      >
+        <Avatar />
+      </motion.div>
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+        {/*Text*/}
+        <div className="flex-1 flex flex-col justify-center">
+          <motion.h2 
+          className="h2"
+          variants={fadeIn("right", 0.6)}
+          initial="hidden"
+          animate="show"
+          exit="hidden">Who am<span className="text-accent"> I </span>?</motion.h2>
+          <motion.p
+          variants={fadeIn("right", 0.6)}
+          initial="hidden"
+          animate="show"
+          exit="hidden" 
+          className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'>
+          
+          Hello, I'm Patrick, a passionate developer based in Québec City with a 
+          deep love for creating innovative applications and games. 
+          I have an AEC in application development and have developed a complete .NET 
+          app for a client, ACEF de Québec, which has further fueled my interest in the latest 
+          frameworks and technologies, especially .NET.
+
+          <br></br>
+          <br></br>
+
+          My journey  has been driven by a desire to bring ideas to life, 
+          whether it's building immersive indie mobile games or crafting productivity apps that make 
+          everyday tasks more engaging. In addition, 
+          I'm a musician and enjoy exploring creative avenues like producing music professionally.
+           
+        </motion.p>
+        {/*Counters*/}
+        <motion.div 
+        variants={fadeIn("right", 0.6)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="hidden md:flex md:max-w-xl xl:max--w-none mx-auto xl:mx-0 mb-8"
+        >
+          <div className="flex flex-1 xl:gap-x-6">
+            {/*Experience*/}
+            <div className="relative flex-1 after:w-[1px] after:h-full
+            after:bg-white/10 after:absolute after:top-0 after:right-0">
+              <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                <CountUp start={0} end={10} duration={5}/> 
+              </div>
+              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-
+              [100px]">Languages & Frameworks</div>
+            </div>
+            {/* clients */}
+            
+            <div className="relative flex-1 after:w-[1px] after:h-full
+            after:bg-white/10 after:absolute after:top-0 after:right-0">
+              <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                <CountUp start={0} end={485} duration={5}/> 
+              </div>
+              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-
+              [100px] ms-1">Github Commits</div>
+            </div>
+            
+            {/* Finished Projects */}
+            <div className="relative flex-1 after:w-[1px] after:h-full
+            after:bg-white/10 after:absolute after:top-0 after:right-0">
+              <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                <CountUp start={0} end={16} duration={5}/> +
+              </div>
+              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-
+              [100px]">Finished Projects</div>
+            </div>
+
+            {/* Statisfied Clients */}
+            <div className="relative flex-1 after:w-[1px] after:h-full
+            after:bg-white/10 after:absolute after:top-0 after:right-0">
+              <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                <CountUp start={0} end={1} duration={5}/>
+              </div>
+              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-
+              [100px]">Statisfied Client</div>
+            </div>
+
+            
+          </div>
+        </motion.div>
+        
+        </div>
+
+        
+        {/*info*/}
+        <motion.div 
+        variants={fadeIn("left", 0.4)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+          {/* Download CV Button */}
+          <motion.div 
+            variants={fadeIn("right", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="mb-4"
+          >
+            <a 
+              href="/PatrickDumoulinCV.pdf" 
+              download 
+              className="btn btn-primary  text-2xl"
+            >
+              <span className="text-accent">&rarr;</span>  Download My CV Here  <span className="text-accent"> &larr; </span>
+            </a>
+          </motion.div>
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+
+            
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className={`${
+                    index === itemIndex &&
+                    "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
+                  } 
+            cursor-pointer capitalize
+            xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute 
+            after:-bottom-1 after:left-0`}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+          <div
+            className=" py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 
+          items-center xl:items-start"
+          >
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div key={itemIndex} 
+                className="flex-1 flex flex-col md:flex-row 
+                max-w-max gap-x-2 items-center text-white/60">
+                  {/*title */}
+                  <div className="font-light mb-2 md:mb-0">{item.title}</div>
+                  <div className="hidden md:flex">-  </div>
+                  <div>{item.stage}</div>
+                  <div className="flex gap-x-4">
+                    {/*Icons */}
+                    {item.icons?.map((icon, itemIndex) => {
+                      return <div className="text-2xl">{icon}</div>;
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- pages/work/index.js
+++ pages/work/index.js
@@ -1,5 +1,96 @@
+// icons
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaWordpress,
+  FaFigma,
+  FaAngular,
+  FaGithub,
+  FaBootstrap,
+  FaVuejs,
+  FaMusic,
+  FaUnity,
+} from "react-icons/fa";
+
+import {
+  SiNextdotjs,
+  SiFramer,
+  SiAdobexd,
+  SiAdobephotoshop,
+  SiCsharp,
+  SiAdobepremierepro,
+  SiMicrosoftword,
+  SiMicrosoftazure,
+  SiReact,
+  SiJavascript,
+  SiTypescript,
+  SiJira,
+  SiAdobeaftereffects,
+  SiBlender,
+  SiXamarin,
+  SiAzuredevops,
+  SiYaml,
+} from "react-icons/si";
+
+import { TbSql } from "react-icons/tb";
+
+export const serviceData = [];
+
+//components
+import WorkSlider from "../../components/WorkSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+
+// framer motion
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          {/* text */}
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <motion.h2
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8"
+            >
+              My Work <span className="text-accent">.</span>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
+              text
+            </motion.p>
+          </div>
+
+          {/* slider */}
+          <motion.div
+            variants={fadeIn("down", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
+          >
+            <WorkSlider />
+          </motion.div>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
 };
 
 export default Work;
+
