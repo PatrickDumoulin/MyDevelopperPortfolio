8f0d8a67670463404158d4b80535679bd7d36e9f
PatrickDumoulin <124711026+PatrickDumoulin@users.noreply.github.com>
2025-02-07 15:19:24 -0500

    Refactor About Page: Enhanced Skills Section and Added Custom Scrollbar

--- pages/about/index.js
+++ pages/about/index.js
@@ -22,17 +22,69 @@ const aboutData = [
   {
     title: "skills",
     info: [
-      { title: "C#", icons: [<SiCsharp key="csharp" />] },
-      { title: "ASP.NET Core | Xamarin | Unity", icons: [<SiCsharp key="aspnet" />, <SiXamarin key="xamarin" />, <FaUnity key="unity" />] },
-      { title: "Angular | React | Next.js | Vue.Js", icons: [<FaAngular key="angular" />, <SiReact key="react" />, <FaVuejs key="vuejs" />] },
-      { title: "SQL Server | PostGres SQL", icons: [<TbSql key="sql" />] },
-      { title: "Azure | DevOps | YAML", icons: [<SiMicrosoftazure key="azure" />, <SiAzuredevops key="devops" />, <SiYaml key="yaml" />] },
-      { title: "JavaScript | TypeScript", icons: [<SiJavascript key="javascript" />, <SiTypescript key="typescript" />] },
-      { title: "HTML | CSS | SASS", icons: [<SiHtml5 key="html5" />, <SiCss3 key="css3" />, <FaSass key="sass" />] },
-      { title: "Git | Github", icons: [<FaGithub key="github" />] },
-      { title: "Agile | Jira", icons: [<SiJira key="jira" />] },
-      { title: "BootStrap", icons: [<FaBootstrap key="bootstrap" />] },
-      { title: "PremierePro | PhotoShop | AfterEffects", icons: [<SiAdobepremierepro key="premiere" />, <SiAdobephotoshop key="photoshop" />, <SiAdobeaftereffects key="aftereffects" />] },
+      {
+        title: "Backend Development",
+        skills: [
+          "C#",
+          "ASP.NET Core, Entity Framework Core",
+          "SQL Server, PostgreSQL",
+          "REST APIs",
+          "Docker"
+        ]
+      },
+      {
+        title: "Frontend Development",
+        skills: [
+          "JavaScript, TypeScript",
+          "Angular, React, Next.js, Vue.js",
+          "HTML, CSS, SASS",
+          "Tailwind CSS, Bootstrap",
+          "WordPress"
+        ]
+      },
+      {
+        title: "Mobile Development",
+        skills: [
+          "React-Native, Native-Wind",
+          "Maui, Xamarin",
+          "Expo"
+        ]
+      },
+      {
+        title: "AI",
+        skills: [
+          "MLNet",
+          "GPT-4, Claude 3.5 Sonnet",
+          "Cursor",
+        ]
+      },
+      {
+        title: "Cloud & DevOps",
+        skills: [
+          "Azure, DevOps",
+          "YAML, Git, Github",
+          "Agile, Jira"
+        ]
+      },
+      {
+        title: "Game Development",
+        skills: [
+          "Unity",
+          "Unreal Engine",
+          "C++"
+        ]
+      },
+      {
+        title: "Creative Tools",
+        skills: [
+          "Premiere Pro",
+          "Photoshop",
+          "After Effects",
+          "Figma",
+          "Blender",
+          "Ableton Live",
+        ]
+      },
     ],
   },
   {
@@ -52,7 +104,7 @@ const aboutData = [
 const About = () => {
   const [index, setIndex] = useState(0);
   return (
-    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left overflow-y-auto">
       <Circles />
       <motion.div
         variants={fadeIn("right", 0.2)}
@@ -65,9 +117,6 @@ const About = () => {
       </motion.div>
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
         <div className="flex-1 flex flex-col justify-center">
-
-
-          {/* Ajoute l'image ici */}
           <div className="mb-4 mt-12">
             <Image
               src="/Maphoto.jpg" // Remplace ce chemin par celui de ta photo
@@ -106,58 +155,48 @@ const About = () => {
             exit="hidden"
             className="hidden md:flex md:max-w-xl xl:max--w-none mx-auto xl:mx-0 mb-8"
           >
-            {/*Counters*/}
-        <motion.div 
-        variants={fadeIn("right", 0.6)}
-        initial="hidden"
-        animate="show"
-        exit="hidden"
-        className="hidden md:flex md:max-w-xl xl:max--w-none mx-auto xl:mx-0 mb-8"
-        >
-          <div className="flex flex-1 xl:gap-x-6">
-            {/*Experience*/}
-            <div className="relative flex-1 after:w-[1px] after:h-full
-            after:bg-white/10 after:absolute after:top-0 after:right-0">
-              <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                <CountUp start={0} end={10} duration={5}/> 
+            <motion.div 
+            variants={fadeIn("right", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden md:flex md:max-w-xl xl:max--w-none mx-auto xl:mx-0 mb-8"
+            >
+              <div className="flex flex-1 xl:gap-x-6">
+                <div className="relative flex-1 after:w-[1px] after:h-full
+                after:bg-white/10 after:absolute after:top-0 after:right-0">
+                  <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                    <CountUp start={0} end={10} duration={5}/> 
+                  </div>
+                  <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-
+                  [100px]">Languages & Frameworks</div>
+                </div>
+                <div className="relative flex-1 after:w-[1px] after:h-full
+                after:bg-white/10 after:absolute after:top-0 after:right-0">
+                  <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                    <CountUp start={0} end={524} duration={5}/> 
+                  </div>
+                  <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-
+                  [100px] ms-1">Github Commits</div>
+                </div>
+                <div className="relative flex-1 after:w-[1px] after:h-full
+                after:bg-white/10 after:absolute after:top-0 after:right-0">
+                  <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                    <CountUp start={0} end={16} duration={5}/> +
+                  </div>
+                  <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-
+                  [100px]">Finished Projects</div>
+                </div>
+                <div className="relative flex-1 after:w-[1px] after:h-full
+                after:bg-white/10 after:absolute after:top-0 after:right-0">
+                  <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                    <CountUp start={0} end={1} duration={5}/>
+                  </div>
+                  <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-
+                  [100px]">Statisfied Client</div>
+                </div>
               </div>
-              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-
-              [100px]">Languages & Frameworks</div>
-            </div>
-            {/* clients */}
-            
-            <div className="relative flex-1 after:w-[1px] after:h-full
-            after:bg-white/10 after:absolute after:top-0 after:right-0">
-              <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                <CountUp start={0} end={524} duration={5}/> 
-              </div>
-              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-
-              [100px] ms-1">Github Commits</div>
-            </div>
-            
-            {/* Finished Projects */}
-            <div className="relative flex-1 after:w-[1px] after:h-full
-            after:bg-white/10 after:absolute after:top-0 after:right-0">
-              <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                <CountUp start={0} end={16} duration={5}/> +
-              </div>
-              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-
-              [100px]">Finished Projects</div>
-            </div>
-
-            {/* Statisfied Clients */}
-            <div className="relative flex-1 after:w-[1px] after:h-full
-            after:bg-white/10 after:absolute after:top-0 after:right-0">
-              <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                <CountUp start={0} end={1} duration={5}/>
-              </div>
-              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-
-              [100px]">Statisfied Client</div>
-            </div>
-
-            
-          </div>
-        </motion.div>
+            </motion.div>
           </motion.div>
         </div>
         <motion.div
@@ -165,7 +204,7 @@ const About = () => {
           initial="hidden"
           animate="show"
           exit="hidden"
-          className="flex flex-col w-full xl:max-w-[48%] h-[480px]"
+          className="flex flex-col w-full xl:max-w-[48%] max-h-[600px] overflow-y-auto custom-scrollbar"
         >
           <motion.div
             variants={fadeIn("right", 0.6)}
@@ -191,15 +230,23 @@ const About = () => {
           </div>
           <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
             {aboutData[index].info.map((item, infoIndex) => (
-              <div key={infoIndex} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60">
-                <div className="font-light mb-2 md:mb-0">{item.title}</div>
-                <div className="hidden md:flex">-</div>
-                <div>{item.stage}</div>
-                <div className="flex gap-x-4">
-                  {item.icons?.map((icon, iconIndex) => (
-                    <div key={iconIndex} className="text-2xl">{icon}</div>
-                  ))}
+              <div key={infoIndex} className="w-full flex flex-col gap-4 mb-8">
+                <div className="text-xl font-semibold text-white mb-2 text-left">
+                  {item.title}
                 </div>
+                <div>{item.stage}</div>
+                {item.skills && (
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-3 w-full">
+                    {item.skills.map((skill, skillIndex) => (
+                      <div 
+                        key={skillIndex} 
+                        className="bg-accent/10 rounded-lg px-4 py-3 flex items-center justify-between hover:bg-accent/20 transition-colors"
+                      >
+                        <span className="text-sm">{skill}</span>
+                      </div>
+                    ))}
+                  </div>
+                )}
               </div>
             ))}
           </div>
