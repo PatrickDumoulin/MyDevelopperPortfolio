f356097121bddda0ea9e4f0264fde9a200deecc9
PatrickDumoulin <124711026+PatrickDumoulin@users.noreply.github.com>
2025-02-11 14:47:33 -0500

    Implement 404 Page for Catch-All Work Route

--- pages/work/[...].js
+++ pages/work/[...].js
@@ -1,7 +1,17 @@
-<div className="h-full bg-gradient-to-b from-[#0B1221] to-[#142237] py-36 flex items-center"> 
-    <div className="container mx-auto">
+import { useRouter } from 'next/router';
+
+const CatchAllPage = () => {
+  const router = useRouter();
+
+  return (
+    <div className="h-full bg-gradient-to-b from-[#0B1221] to-[#142237] py-36 flex items-center">
+      <div className="container mx-auto">
         <h2 className="text-4xl font-bold text-center text-white">
-            Work with me
+          Page not found
         </h2>
+      </div>
     </div>
-</div>
+  );
+};
+
+export default CatchAllPage;
