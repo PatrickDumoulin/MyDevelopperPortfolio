220725f7a27d2d4360cbd3e8bf65ea21dbbf335e
PatrickDumoulin <124711026+PatrickDumoulin@users.noreply.github.com>
2025-02-13 14:30:23 -0500

    Refactor Page Layouts: Improve Responsive Height and Scrolling Behavior

--- pages/work/index.js
+++ pages/work/index.js
@@ -59,46 +59,48 @@ const Work = () => {
   const t = translations[locale];
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-[#0B1221] to-[#142237] py-32 text-center xl:text-left">
-      <Circles />
-      <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row gap-x-8">
-          {/* text */}
-          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-            <motion.h2
-              variants={fadeIn('up', 0.2)}
-              initial='hidden'
-              animate='show'
-              exit='hidden'
-              className='h2 xl:mt-12'
-            >
-              {t.work.title}<span className='text-accent'>.</span>
-            </motion.h2>
-            <motion.p
-              variants={fadeIn('up', 0.4)}
-              initial='hidden'
-              animate='show'
-              exit='hidden'
-              className='mb-4 max-w-[400px] mx-auto lg:mx-0'
-            >
-              {t.work.description}
-            </motion.p>
-          </div>
+    <AnimatePresence>
+      <div className="h-auto md:h-full bg-gradient-to-b from-[#0B1221] to-[#142237] py-32 text-center xl:text-left overflow-y-auto overflow-x-hidden">
+        <Circles />
+        <div className="container mx-auto">
+          <div className="flex flex-col xl:flex-row gap-x-8">
+            {/* text */}
+            <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+              <motion.h2
+                variants={fadeIn('up', 0.2)}
+                initial='hidden'
+                animate='show'
+                exit='hidden'
+                className='h2 xl:mt-12'
+              >
+                {t.work.title}<span className='text-accent'>.</span>
+              </motion.h2>
+              <motion.p
+                variants={fadeIn('up', 0.4)}
+                initial='hidden'
+                animate='show'
+                exit='hidden'
+                className='mb-4 max-w-[400px] mx-auto lg:mx-0'
+              >
+                {t.work.description}
+              </motion.p>
+            </div>
 
-          {/* slider */}
-          <motion.div
-            variants={fadeIn('down', 0.6)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
-            className='w-full xl:max-w-[65%]'
-          >
-            <WorkSlider />
-          </motion.div>
+            {/* slider */}
+            <motion.div
+              variants={fadeIn('down', 0.6)}
+              initial='hidden'
+              animate='show'
+              exit='hidden'
+              className='w-full xl:max-w-[65%]'
+            >
+              <WorkSlider />
+            </motion.div>
+          </div>
         </div>
+        <Bulb />
       </div>
-      <Bulb />
-    </div>
+    </AnimatePresence>
   );
 };
 
